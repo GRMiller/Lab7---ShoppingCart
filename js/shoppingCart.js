98ac2a2cee606c8236b129f8d4f18c1a992cc583
Garrett Miller <garrettralphmiller@gmail.com>
2016-11-04 10:47:54 -0400

    Update with Lab 8 Bonus

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -5,15 +5,15 @@ var cart = [];
 
 // DECLARE OBJECTS & PUSH INTO CART
 
-var Item = function (name, cost, qty) {				//CREATE CONSTRUCTOR OBJECT
+var Item = function (name, cost, qty) {			//CREATE CONSTRUCTOR OBJECT
 	this.name = name; 
-	this.cost = Number(cost.toFixed(2)); 
+	this.cost = Number(cost); 
 	this.qty = Number(qty);
-	this.subtotal = function () {
-		var subtotal = Number(this.cost * this.qty).toFixed(2);
+	this.subtotal = function () {              // THE NEXT 3 PROPERTIES ARE DOING NOTHING IN THIS CODE, JUST ME TRYING TO PUSH MYSELF
+		var subtotal = Number(this.cost * this.qty);
 		return subtotal;
 	}
-	this.totalTax = function() {				//USE FUNCTION FOR MATH
+	this.totalTax = function() {						//USE FUNCTION FOR MATH
 		var totalTax = Number(this.subtotal() * 0.06);	//SPECIFY RETURN AS NUMBER
 		return totalTax;
 	}
@@ -28,51 +28,43 @@ var Item = function (name, cost, qty) {				//CREATE CONSTRUCTOR OBJECT
 
 function addItemToCart(name, cost, qty) { 	//NAME FUNCTION AND PARAMETER
 
-	for (var i = 0; i < cart.length; i++) { 	//LOOP THROUGH PROPERTIES OF OBJECTS IN CART
+	for (var i = 0; i < cart.length; i++) { //LOOP THROUGH PROPERTIES OF OBJECTS IN CART
 		
 		if (cart[i].name === name) {	//TO CHECK FOR DUPLICATE ITEMS
 			cart[i].qty += qty;			//INCREASE QTY
-		//	qtyCell.innerHTML = car
 			cart[i].cost += cost;		//FIX THIS LINE, USE SUBTOTALS
+			
 			return; 					//STOP LOOP AND ACT
 		}								//END FUNCTION when IF = TRUE
 	}									//END LOOP
 
-	var item = new Item (name, cost, qty); //DECLARE NEW OBJECT IF NOT DUPLICATE
-	cart.push(item);					//AND PUSH NEW ITEM INTO CART 
+		cart.push(new Item (name, cost, qty));		//DECLARE NEW OBJECT IF NOT DUPLICATE AND PUSH TO CART
+						
 	
 
-	var table = document.getElementById('cart_table');   //GET ALL PROPERTIES OF ITEM TO REPLACE
-	var row = table.insertRow(1);		// CREATE NEW ROW AND CELLS FOR NEW CART ITEM
+	var table = document.getElementById('cart_table');  	//GET ALL PROPERTIES OF ITEM TO REPLACE
+	var row = table.insertRow(1);							// CREATE NEW ROW AND CELLS FOR NEW CART ITEM
 	var nameCell = row.insertCell(0);	
 	var costCell = row.insertCell(1);
 	var qtyCell = row.insertCell(2);
 
-	// for (var i = 0; i<cart.length; i++) {
-	// 	if (cart[i].name ===name) {
-	// 		costcell.innerHTML = cart[i].cost += cost;
-	// 		qtyCell.innerHTML = cart[i].qty += qty;
-	// 		return;
-	// 	}
-	// }									//ADD ITEMS INTO TABLE on PAGE
+	/*for (var i = 0; i<cart.length; i++) {    //CREATE LOOP TO DISPLAY UPDATED QTY AND COST IN TABLE
+		if (cart[i].name ===name) {
+			costcell.innerHTML = cart[i].cost += cost;
+			qtyCell.innerHTML = cart[i].qty += qty;
+			return;
+		}
+	}		*/							
+
+	//ADD ITEMS INTO TABLE on PAGE
 	
 	nameCell.innerHTML = cart[i].name;
-	costCell.innerHTML = cart[i].cost;
+	costCell.innerHTML = cart[i].cost.toFixed(2);
 	qtyCell.innerHTML = cart[i].qty;
+
 }; 
 
 
-
-//LOOP THROUGH CART TO PRINT NAME, PRICE, QTY INTO TABLE
-
-// function showCart () {
-// 	var table = document.getElementById('cart_table');
-// 	for (var i in cart) {
-// 		document.write ( "Your " + cart[i].qty + " " + cart[i].name + " will cost you $" + (cart[i].cost).toFixed(2) + " ")
-// 	}
-
-// 	return;
-// };
 
 
 
@@ -87,10 +79,24 @@ function totalCart () {
 	var eltotal_cell = document.getElementById('total_cell');
 	eltotal_cell.innerHTML = totalCartCost;
 
-	// + totalCartCost.toFixed(2) + ". Thank you for shopping with us. Refresh to go back to item menu and add your items in again.")
-	return totalCartCost;
+	
+	return totalCartCost.toFixed(2);
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+// var add_to_cart_btn = document.getElementsByClass ('add_to_cart');
+// add_to_cart_btn.addEventListener ('click', addItemToCart()); 
 
 //removeItemFromCart(name) // Removes one item
 
